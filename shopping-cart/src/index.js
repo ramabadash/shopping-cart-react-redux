e20b1752dc88065547b10a9e3ac0d26c4f5c66cb
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-20 16:16:46 +0200

    # Create store from mainReducer and provide from index.js

--- shopping-cart/src/index.js
+++ shopping-cart/src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import rootReducer from './reducers/mainReducer';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
