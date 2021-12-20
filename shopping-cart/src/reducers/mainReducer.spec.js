02957f9132afadc6f74e4372013dbaa6b838fb2c
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-20 19:47:16 +0200

    # Test main reducer - "should not add item with quantity 0" test

--- shopping-cart/src/reducers/mainReducer.spec.js
+++ shopping-cart/src/reducers/mainReducer.spec.js
@@ -42,6 +42,17 @@ const afterAddToCartState = {
   ],
 };
 
+const finishedProductState = {
+  products: [
+    { id: 1, name: 'Jeans', price: 50.01, quantity: 8 },
+    { id: 2, name: 'T- shirt', price: 10.99, quantity: 10 },
+    { id: 3, name: 'Coat', price: 80.99, quantity: 5 },
+    { id: 4, name: 'Shoes', price: 100.99, quantity: 0 },
+  ],
+  totalPrice: 504.95,
+  shoppingCart: [{ id: 4, name: 'Shoes', quantity: 5 }],
+};
+
 describe('main reducer', () => {
   it('should handle initial state', () => {
     expect(mainReducer(undefined, {})).toEqual(primaryState);
@@ -53,9 +64,16 @@ describe('main reducer', () => {
     );
   });
 
-  it('should handle ADD_TO_CART', () => {
-    expect(
-      mainReducer(midShoppingState, { type: 'ADD_TO_CART', id: 4 })
-    ).toEqual(afterAddToCartState);
+  describe('should handle ADD_TO_CART', () => {
+    it('should add one item', () => {
+      expect(
+        mainReducer(midShoppingState, { type: 'ADD_TO_CART', id: 4 })
+      ).toEqual(afterAddToCartState);
+    });
+    it('should not add item with quantity 0', () => {
+      expect(
+        mainReducer(finishedProductState, { type: 'ADD_TO_CART', id: 4 })
+      ).toEqual(finishedProductState);
+    });
   });
 });
