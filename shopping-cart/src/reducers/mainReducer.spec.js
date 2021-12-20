a38d01c9f835e155b3b508a21a190750414f42ca
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-20 19:32:39 +0200

    # Test main reducer - "should handle BUY'" test

--- shopping-cart/src/reducers/mainReducer.spec.js
+++ shopping-cart/src/reducers/mainReducer.spec.js
@@ -1,8 +1,40 @@
 import mainReducer from './mainReducer';
+
+/*****  STATES : PRIMARY , FULL CART, AFTER BUYING FULL CART *****/
 import { primaryState } from './mainReducer';
+const midShoppingState = {
+  products: [
+    { id: 1, name: 'Jeans', price: 50.01, quantity: 6 },
+    { id: 2, name: 'T- shirt', price: 10.99, quantity: 5 },
+    { id: 3, name: 'Coat', price: 80.99, quantity: 5 },
+    { id: 4, name: 'Shoes', price: 100.99, quantity: 5 },
+  ],
+  totalPrice: 154.97,
+  shoppingCart: [
+    { id: 1, name: 'Jeans', quantity: 2 },
+    { id: 2, name: 'T- shirt', quantity: 5 },
+  ],
+};
+
+const afterBuyState = {
+  products: [
+    { id: 1, name: 'Jeans', price: 50.01, quantity: 6 },
+    { id: 2, name: 'T- shirt', price: 10.99, quantity: 5 },
+    { id: 3, name: 'Coat', price: 80.99, quantity: 5 },
+    { id: 4, name: 'Shoes', price: 100.99, quantity: 5 },
+  ],
+  totalPrice: 0,
+  shoppingCart: [],
+};
 
 describe('main reducer', () => {
   it('should handle initial state', () => {
     expect(mainReducer(undefined, {})).toEqual(primaryState);
+  });
+
+  it('should handle BUY', () => {
+    expect(mainReducer(midShoppingState, { type: 'BUY' })).toEqual(
+      afterBuyState
+    );
   });
 });
